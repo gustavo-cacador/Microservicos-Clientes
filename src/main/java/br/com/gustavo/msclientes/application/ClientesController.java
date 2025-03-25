eea3be8633e5cd38d695162e08ffaf68f0192fed
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-03-24 23:49:56 -0300

    Concluindo e testando o microservice de clientes

--- src/main/java/br/com/gustavo/msclientes/application/ClientesController.java
+++ src/main/java/br/com/gustavo/msclientes/application/ClientesController.java
@@ -1,15 +1,47 @@
 package br.com.gustavo.msclientes.application;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.gustavo.msclientes.application.representation.ClienteSaveRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClientesController {
+
+    private final ClienteService service;
 
     @GetMapping
     public String status() {
         return "ok";
+    }
+
+    // metodo para salvar dados do cliente, com ClienteSaveRequest (cpf, nome e idade)
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request) {
+        var cliente = request.toModel();
+        service.save(cliente);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf) {
+        var cliente = service.getByCPF(cpf);
+
+        // lógica para verificar se cpf existe
+        // pois pode acontecer de cliente passar cpf que n esteja cadastrado no sistema
+        if (cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 }
---
+++ src/main/java/br/com/gustavo/msclientes/application/representation/ClienteSaveRequest.java
@@ -0,0 +1,16 @@
+package br.com.gustavo.msclientes.application.representation;
+
+import br.com.gustavo.msclientes.domain.Cliente;
+import lombok.Data;
+
+@Data
+public class ClienteSaveRequest {
+
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel() {
+        return new Cliente(cpf, nome, idade);
+    }
+}
